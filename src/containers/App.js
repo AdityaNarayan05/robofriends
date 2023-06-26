fcaafe8ea3339f0f8f2ccfa424a2cf2ad18e6304
Aditya Narayan Jaiswal <nlaxmi886@gmail.com>
2023-06-26 15:53:26 +0530

    some final Changes

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { setSearchField, requestRobots } from '../actions';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
-import { setSearchField,requestRobots } from '../actions';
-
-const mapStateToProps = state => {
+// parameter state comes from index.js provider store state(rootReducers)
+const mapStateToProps = (state) => {
   return {
     searchField: state.searchRobots.searchField,
     robots: state.requestRobots.robots,
-    isPending:state.requestRobots.isPending,
-    error: state.requestRobots.error
+    isPending: state.requestRobots.isPending
   }
 }
 
+// dispatch the DOM changes to call an action. note mapStateToProps returns object, mapDispatchToProps returns function
+// the function returns an object then uses connect to change the data from reducers.
 const mapDispatchToProps = (dispatch) => {
   return {
     onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
-    onRequestRobots : ()=>dispatch(requestRobots)
+    onRequestRobots: () => dispatch(requestRobots())
   }
 }
 class App extends Component {
@@ -28,22 +30,25 @@ class App extends Component {
   }
 
   render() {
-    const { searchField, onSearchChange, robots, isPending } =this.props;
-    const filteredRobots = robots.filter(robot =>{
+    const { robots, searchField, onSearchChange, isPending } = this.props;
+    const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
     })
-    return isPending ?
-      <h1>Loading</h1> :
-      (
-        <div className='tc'>
-          <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={onSearchChange}/>
-          <Scroll>
-            <CardList robots={filteredRobots} />
-          </Scroll>
-        </div>
-      );
+    return (
+      <div className='tc'>
+        <h1 className='f1'>RoboFriends</h1>
+        <SearchBox searchChange={onSearchChange}/>
+        <Scroll>
+          { isPending ? <h1>Loading</h1> :
+            <ErrorBoundry>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundry>
+          }
+        </Scroll>
+      </div>
+    );
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+// action done from mapDispatchToProps will change state from mapStateToProps
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
 import React from 'react';
 // import ReactDOM from 'react-dom'; The new way to import createRoot:
 import { createRoot } from "react-dom/client";
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { createStore,applyMiddleware,combineReducers } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
-import  ThunkMiddleware  from 'redux-thunk';
-import './index.css';
-import App from './containers/App';
-import { searchRobots,requestRobots } from './reducers';
 import 'tachyons';
+import App from './containers/App';
 // import registerServiceWorker from './registerServiceWorker';
+import { requestRobots, searchRobots } from './reducers'
+import './index.css';
 
-const logger=createLogger();
-
-const rootReducer=combineReducers({searchRobots,requestRobots});
-const store = createStore(rootReducer ,applyMiddleware(ThunkMiddleware,logger));
-
+const logger = createLogger() 
+const rootReducers = combineReducers({requestRobots, searchRobots})
+const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger))
 const root = createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={store}>
-        <App />
-        </Provider>);
+<Provider store={store}>
+  <App/>
+</Provider>
+);
 
-
-// ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App/>
+//   </Provider>,
+//   document.getElementById('root')
+// );
 // registerServiceWorker();
