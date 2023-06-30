af1f89b06fbf50535a34f437c4bd52c363523c66
Aditya Narayan Jaiswal <nlaxmi886@gmail.com>
2023-06-30 14:44:30 +0530

    Transforming it into PWA

--- src/index.js
+++ src/index.js
@@ -6,14 +6,20 @@ import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import 'tachyons';
+
+
 import App from './containers/App';
-// import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { requestRobots, searchRobots } from './reducers'
+
 import './index.css';
 
 const logger = createLogger() 
+
 const rootReducers = combineReducers({requestRobots, searchRobots})
+
 const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger))
+
 const root = createRoot(document.getElementById('root'));
 root.render(
 <Provider store={store}>
@@ -27,4 +33,8 @@ root.render(
 //   </Provider>,
 //   document.getElementById('root')
 // );
-// registerServiceWorker();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
